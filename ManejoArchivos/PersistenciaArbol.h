1f2dd9c9ffaa803a19d2898d888cbfa0d18fa6b1
leandro.rodriguezg@gmail.com <leandro.rodriguezg@gmail.com@1b4db699-83f3-5de1-985d-e0b4d965300a>
2012-05-19 22:26:24 +0000

    persistencia. falta retocarla para que ahora guarde en 2 archivos

---
+++ ManejoArchivos/PersistenciaArbol.h
@@ -0,0 +1,65 @@
+/*
+ * PersistenciaIndices.h
+ *
+ *  Created on: Sep 22, 2010
+ *      Author: peta
+ */
+
+#ifndef PERSISTENCIAINDICES_H_
+#define PERSISTENCIAINDICES_H_
+
+#include <iostream>
+#include <fstream>
+#include "string"
+#include "exception"
+#include "iostream"
+
+#include "Arbol/Nodo.h"
+#include "Arbol/NodoHoja.h"
+#include "Arbol/NodoInterno.h"
+#include "../Constantes.h"
+
+using namespace std;
+class PersistenciaArbol {
+
+protected:
+	/*aca tengo el archivo con el que interactuo (el de nodos)*/
+	fstream archivo;
+	/*tendria que poner otro para el de datos?*/
+
+	/*devuelvo el nro de nodo correspondiente para un nuevo nodo (autoincremental)=>(ultimoNroNodo+1). devuelvo -1 si*/
+	offset NroNodoNuevo();
+
+public:
+	/*creo o abro el archivo al final, para editarlo*/
+	PersistenciaArbol(string nombreArchivo);
+	/*cierro el archivo, si esta abierto*/	
+	virtual ~PersistenciaArbol();
+
+	/*guardo un nodo nuevo. Si pudo insertar retorno el nro de nodo*/
+	offset agregarNodo(Nodo* nodo);
+	/*devuelvo true si pude actualizar, false si no*/
+	bool ActualizarNodo(Nodo* nodo);
+	/*devuelvo true si pude actualizar, false si no*/
+	bool guardarRaiz(Nodo* nodo);
+	/*devuelvo puntero al nodo raiz del archivo*/
+	Nodo* obtenerRaiz();
+	/*me pasan un nro de nodo y devuelvo un puntero a ese nodo si existe. si hay algun error devuelvo null*/
+	Nodo* leerNodo(offset nroNodo);
+	/*si el archivo fue abierto, me fijo si esta vacio*/
+	bool isVacio();
+	/*me fijo si esta el nro de bloque buscado*/
+	bool existeBloque(offset nro);
+
+
+	// el bloque 0 para metadatos.
+	/*devuelve un string de metadatos(bloque 0)*/
+	char* leerMetaDatos();
+	/*guardo los metadatos del arbol. Devuelve true si se pudo guardar, false si no*/
+	bool cargarMetaDatos(char* metaData, long int tamanio);
+
+
+
+};
+
+#endif /* PERSISTENCIAINDICES_H_ */
