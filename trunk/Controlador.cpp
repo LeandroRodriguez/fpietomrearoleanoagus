3367ea8efaa72df24b4da4bebe31f0ca97aa23fa
leandro.rodriguezg@gmail.com <leandro.rodriguezg@gmail.com@1b4db699-83f3-5de1-985d-e0b4d965300a>
2012-05-22 03:15:32 +0000

    controladores incompletos

--- trunk/Controlador.cpp
+++ trunk/Controlador.cpp
@@ -1,5 +1,76 @@
 #include "Controlador.h"
 
+/*REVISAR Y ARREGLAR*/
+Controlador::Controlador() {
+	 ManipuladorArchivos* manipulo = new ManipuladorArchivos();
+	 manipulo->creoCarpeta("./Indices");
+	 manipulo->creoCarpeta("./Archivos");
+	 manipulo->creoCarpeta("./Indices/Seleccion");
+	 manipulo->creoCarpeta("./Indices/Clasificacion");//nosotros solo necesitamos una de estas 2 creo
+
+	 archivosClasificacion
+		 = manipulo->getArchivos(DIR_ARCHIVO_INDICES_CLASIFICACION,"",".idx");
+
+	 archivosSeleccion
+		 = manipulo->getArchivos(DIR_ARCHIVO_INDICES_SELECCION,"",".idx");
+
+	 vector<string> aux = manipulo->getArchivos(DIR_ARCHIVO_INDICES,"",".idx");
+
+	 if (aux.size()>0){
+		 archivoPrimario = aux[0];
+	 }else{
+		 archivoPrimario = ARCHIVO_INDICE_PRIMARIO;
+		 archivoPrimario += ".idx";
+	 }
+
+	 delete manipulo;
+}
+
+Controlador::~Controlador(){
+	// TODO Auto-generated destructor stub
+}
+
+/*Inserto el dato pasado por parametro tanto en el archivo de data, como sus referencias en el arbol*/
+void Controlador::InsertarDato(Key* dato){
+	/*creo un objeto que se encargara de almacenar el dato en el archivo correspondiente*/
+	AlmacenamientoBloque almacena(ARCHIVO_DATOS, ARCHIVO_DATOS_LIBRES);
+	/*creo el indice para guardar las referencias al dato en mi arbol*/
+	Indice* indicePrimario = new Indice(this->archivoPrimario);
+
+	/*si el dato no es null, lo guardo*/
+	if (dato) {
+		/*obtengo los bytes del dato serializado*/
+		Bytes bytes(dato->serializarse());
+		/*creo un registro donde guardo los bytes del dato, y lo guardo en el archivo de data*/
+		RegistroVariable registro(bytes);
+		almacena.agregarRegistro(&registro);
+		/*le paso al arbol el numero de bloque y el UID del reg en donde guarde el dato*/
+		indicePrimario->agregarDato(almacena.getNumeroBloque(), almacena.getNumeroUID(), dato);
+
+		/*borro el indice primario*/
+		delete indicePrimario;//linea peligrosa quizas, mirarla en caso de que algo falle
+	}
+}
+
+
+/*
+void Controlador::cargarArchivos(){
+
+	 ManipuladorArchivos* manipulo = new ManipuladorArchivos();
+
+	 archivosClasificacion
+		 = manipulo->getArchivos(DIR_ARCHIVO_INDICES_CLASIFICACION,"",".idx");
+
+	 archivosSeleccion
+		 = manipulo->getArchivos(DIR_ARCHIVO_INDICES_SELECCION,"",".idx");
+
+	 archivoPrimario
+		 = manipulo->getArchivos(DIR_ARCHIVO_INDICES,"",".idx")[0];
+
+	 delete manipulo;
+}*/
+
+
 /*Controlador::Controlador(){
     //not implemented
     }
--- trunk/Controlador.h
+++ trunk/Controlador.h
@@ -1,18 +1,71 @@
 #ifndef CONTROLADOR_H_
 #define CONTROLADOR_H_
 
+#include "Constantes.h"
+#include "Datos/Bytes.h"
+#include "Almacenamiento/Almacenamiento.h"
+#include "Almacenamiento/AlmacenamientoBloque.h"
+#include "Datos/Bloque.h"
+#include "Datos/Registro.h"
+#include "Datos/RegistroVariable.h"
+
+//#include "Datos/ClaveNumero.h"
+//#include "Datos/Clave.h"
+#include "AccesoAMails/AccesoAEmails.h"
+#include "Indices/Indice.h"
+#include "Indices/IndiceSeleccion.h"
+#include "Indices/IndiceClasificacion.h"
+
+#include "Controlador.h"
+#include "exception"
+#include "string"
+#include "iostream"
+#include <iostream>
+#include "ManipuladorArchivos.h"
+
 using namespace std;
 
-class Controlador{
-    public:
-    Controlador();
-    /*ListarTenesPorFalla(falla);
-    ListarTenesPorAccidente(accidente);
-    ListarFallasPorFormacion(formacion);
-    ListarAccidentesPorFormacion(formacion);
-    ListarFallas();
-    ListarForcaciones();
-    ListarLineas();
-    ListarAccidentes();*/
+class Controlador {
+
+	vector<string> archivosClasificacion;
+	vector<string> archivosSeleccion;
+	string archivoPrimario;/*nombre del archivo con el indice(mi arbol)*/
+
+public:
+
+	// Crea el controlador y levanta todos los indices en /Archivos/Indices/
+	Controlador();
+	virtual ~Controlador();
+
+	/*Inserto el dato pasado por parametro tanto en el archivo de data, como sus referencias en el arbol*/
+	void InsertarDato(Key* dato);
+
+	/*ListarTenesPorFalla(falla);
+	ListarTenesPorAccidente(accidente);
+	ListarFallasPorFormacion(formacion);
+	ListarAccidentesPorFormacion(formacion);
+	ListarFallas();
+	ListarForcaciones();
+	ListarLineas();
+	ListarAccidentes();*/
+
+
+protected:
+
+
+	//REUTILIZABLE? -> void cargarArchivos();
+
+	// Obsoletos?...
+	/*
+	void DescargoMailsCreoIndice(TipoClasificacion tipo,string palabra);
+	void DescargoMailsCreoIndiceFrom(string from);
+	void DescargoMailsCreoIndiceTo(string to);
+	void DescargoMailsCreoIndiceSubject(string subject);
+	void DescargoMailsCreoIndiceClasificacion(TipoClasificacion tipo);
+	void DescargoMailsCreoIndice();
+	void MailsCreoIndice(string palabra);
+	*/
+	// ...Fin Obsoletos?
 };
-#endif
+
+#endif /* CONTROLADOR_H_ */
